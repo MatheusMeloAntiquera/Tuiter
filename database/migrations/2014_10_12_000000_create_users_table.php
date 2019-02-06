ac10fa9732f72cb56b3c5897eab04ff618bd4444
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2019-02-06 07:30:39 -0200

    Api Auth with Passport Part 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->date('birth_date');
-            $table->string('bio', 200);
+            $table->date('birth_date')->nullable();
+            $table->string('bio', 200)->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->engine = 'InnoDB';
